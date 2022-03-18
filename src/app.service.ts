69f8a26cc796d36d44eee54ddc39b735cb46acf2
eyders <eyders@gmail.com>
2022-03-17 23:35:17 -0500

    Configuracion de Entorno

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,17 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    private configService: ConfigService,
   ) {}
 
   getHello(): string {
-    //console.log(this.tasks);
-    return `Hello World! ${this.apiKey}`;
+    const apiKey = this.configService.get('API_KEY');
+    const name = this.configService.get('DATABASE_NAME');
+    return `Hello World! ${apiKey} ${name}`;
   }
 }
