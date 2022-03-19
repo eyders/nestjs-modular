0f9c208e59954fb8efb6f32e0d0639c1bf7bb2a1
eyders <eyders@gmail.com>
2022-03-18 23:16:04 -0500

    Configuracion de Entorno Final

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,21 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigType, ConfigService } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
-    private configService: ConfigService,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>
   ) {}
-
+  //private configService: ConfigService,
   getHello(): string {
-    const apiKey = this.configService.get('API_KEY');
-    const name = this.configService.get('DATABASE_NAME');
+    const apiKey = this.configService.apiKey;
+    const name = this.configService.database.name;
+
+    // const apiKey = this.configService.get('API_KEY');
+    // const name = this.configService.get('DATABASE_NAME');
     return `Hello World! ${apiKey} ${name}`;
   }
 }
